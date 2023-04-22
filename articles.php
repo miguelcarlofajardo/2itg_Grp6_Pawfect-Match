0ad76e64f41c3cedc2baef8b41c6d7516c071c31
Miguel Fajardo <miguelcarlo.fajardo.cics@ust.edu.ph>
2023-04-22 18:07:23 +0800

    Body of page. Styles not yet finished. Miguel-4/22

---
+++ articles.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Articles</title>
+    <link rel="icon" type="image/x-icon" href="./assets/images/favicon-logo.png">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.css">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body data-bs-spy="scroll" data-bs-target=".navbar">
+<!--navbar-->
+
+
+<!--news-->
+<section id="news" class="section-padding">
+    <div class="col-12 text-center" data-aos="fade-down" data-aos-delay="150">
+        <h1> How to take care of your new pet? </h1>
+        <p>Articles</p>
+        <br></br>
+            <div class="container-c">
+            <div class="text"> 
+                <img src="./images/article-image-1.png" alt="">
+                <h3> Everything a First-Time Dog Owner Should Know </h3> 
+                <h6> Published on September 23, 2022 by Jackie Brown</h6>
+                    <p>
+                        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur '     
+                        <a href="https://www.thesprucepets.com/everything-a-first-time-dog-owner-should-know-5667284" target="_blank" class="link"><u>Know more!</u></a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    <br>
+    <div class="col-12 text-center" data-aos="fade-down" data-aos-delay="150">
+        <div class="container-c">
+            <div class="text"> 
+                <img src="./images/article-image-2.png" alt="">
+                <h3> 10 Responsible Pet Care Tips </h3> 
+                <h6> Published on August 2, 2018 by  Christine O'Brien </h6>
+                    <p>
+                        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur '
+                        <a href="https://www.hillspet.com/pet-care/routine-care/10-responsible-pet-care-tips?lightboxfired=true#" target="_blank" class="link"><u>Know more></u></a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    <br>
+    <div class="col-12 text-center" data-aos="fade-down" data-aos-delay="150">
+        <div class="container-c">
+            <div class="text">
+                <img src="./images/article-image-3.jpg" alt=""> 
+                <h3> The Basic Necessities of Proper Pet Care </h3> 
+                <h6> Published on January 23,2018 by The Animal Foundation </h6>
+                    <p>
+                        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur '
+                        <a href="https://animalfoundation.com/whats-going-on/blog/basic-necessities-proper-pet-care" target="_blank" class="link"><u>Know more></u></a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    <br>
+    <div class="col-12 text-center" data-aos="fade-down" data-aos-delay="150">
+         <div class="container-c">
+            <div class="text">
+                <img src="./images/article-image-4.png" alt=""> 
+                <h3> 18 essential tips for first-time pet owners </h3> 
+                <h6> Updated on April 19, 2023 by Amy McCarthy </h6>
+                    <p>
+                        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur '
+                        <a href="https://www.yardbarker.com/lifestyle/articles/18_essential_tips_for_first_time_pet_owners/s1__35769011#slide_4" target="_blank" class="link"><u>Know more></u></a>
+                    </p>
+                </div>
+             </div>
+        </div>
+    <br>
+    <div class="col-12 text-center" data-aos="fade-down" data-aos-delay="150">
+        <div class="container-c">
+            <div class="text">
+                <img src="./images/article-image-5.png" alt=""> 
+                <h3> The Basic Necessities of Proper Pet Care </h3> 
+                <h6> Puslished by Tuft + Paw </h6>
+                    <p>
+                        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur '
+                        <a href="https://www.tuftandpaw.com/blogs/cat-guides/the-definitive-guide-to-cat-behavior-and-body-language" target="_blank" class="link"><u>Know more></u></a>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js"></script>
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script src="./assets/js/main.js"></script>
+</body>
+
+</html>
---
+++ videos.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Videos</title>
+    <link rel="icon" type="image/x-icon" href="./assets/images/favicon-logo.png">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.css">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body data-bs-spy="scroll" data-bs-target=".navbar">
+
+<!--navbar-->
+
+
+<!--video-->
+<section id="videos" class="section-padding">
+    <br></br>
+    <div class="col-12 text-center" data-aos="fade-down" data-aos-delay="150">
+        <h1> How to take care of your new pet? </h1>
+        <p>Videos</p>
+        <div class="container-d">
+            <div class="card">
+                <iframe src="https://www.youtube.com/watch?v=peUVLEUj-AM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <div class="card-body">
+                    <h5 class="card-title">Before getting a puppy</h5>
+                    <p class="card-text">Things to know before getting a puppy by Doctor Mike </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 text-center" data-aos="fade-down" data-aos-delay="150">
+        <div class="container-d">        
+            <div class="card">
+                <iframe src="https://www.youtube.com/watch?v=Yzv0gXqoCkc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <div class="card-body">
+                    <h5 class="card-title">The Basics</h5>
+                    <p class="card-text">Pet Care Basics by MiamiDadeCounty</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 text-center" data-aos="fade-down" data-aos-delay="150">
+        <div class="container-d">
+            <div class="card">
+                <iframe src="https://www.youtube.com/watch?v=Tn3lZE0rRBs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <div class="card-body">
+                    <h5 class="card-title">Guide to Puppy Care </h5>
+                    <p class="card-text"> A video on how to take care of a puppy by AnimalWised </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 text-center" data-aos="fade-down" data-aos-delay="150">
+        <div class="container-d">
+            <div class="card">
+                <iframe src="https://www.youtube.com/watch?v=Tn3lZE0rRBs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <div class="card-body">
+                    <h5 class="card-title"> Don't do these things </h5>
+                    <p class="card-text"> A video on how to take care of a puppy by AnimalWised </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 text-center" data-aos="fade-down" data-aos-delay="150">
+        <div class="container-d">
+            <div class="card">
+                <iframe src="https://www.youtube.com/watch?v=jSC3OMz78EQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <div class="card-body">
+                    <h5 class="card-title">Not realizing you're already hurting youre dog </h5>
+                    <p class="card-text"> 16 ways you are hurting your dog without realizing it by Jaw-Dropping Facts </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<!--footer-->
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js"></script>
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script src="./assets/js/main.js"></script>
+</body>
+
+</html>
