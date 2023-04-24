6a8f47d3d9125256c6f895a306c306adc55347b8
Miguel Fajardo <miguelcarlo.fajardo.cics@ust.edu.ph>
2023-04-24 17:08:17 +0800

    updated article and video page. Miguel - 4/24

---
+++ article/video.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pawfect Match | Videos</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../css/video.css">
+    <link rel="shortcut icon" href="img/pawfectmatchlogo.png">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@700&family=Nunito&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet"> 
+</head>
+
+<body>
+
+<!--navbar-->
+<?php require_once "../components/navbar.php"; ?>
+
+<!--video-->
+<section id="videos" class="section-padding">
+    <br></br>
+    <div class="col-12 text-center">
+        <h1> How to take care of your new pet? </h1>
+        <h2>Videos</h2>
+        <div class="container-video">
+            <div class="card">
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/peUVLEUj-AM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <div class="card-body">
+                    <h5 class="card-title">Before getting a puppy</h5>
+                    <p class="card-text">Things to know before getting a puppy by Doctor Mike </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 text-center">
+        <div class="container-video">        
+            <div class="card">
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/Yzv0gXqoCkc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <div class="card-body">
+                    <h5 class="card-title">The Basics</h5>
+                    <p class="card-text">Pet Care Basics by MiamiDadeCounty</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 text-center">
+        <div class="container-video">
+            <div class="card">
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/Tn3lZE0rRBs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <div class="card-body">
+                    <h5 class="card-title">Guide to Puppy Care </h5>
+                    <p class="card-text"> A video on how to take care of a puppy by AnimalWised </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 text-center">
+        <div class="container-video">
+            <div class="card">
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/DCmaoBujsAI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <div class="card-body">
+                    <h5 class="card-title"> Don't do these things </h5>
+                    <p class="card-text"> Here are some mistakes that may shorten your pet's life by BRIGHT SIDE </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 text-center">
+        <div class="container-video">
+            <div class="card">
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/jSC3OMz78EQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <div class="card-body">
+                    <h5 class="card-title">Not realizing you're already hurting youre dog </h5>
+                    <p class="card-text"> Ways you are hurting your dog without realizing it by Jaw-Dropping Facts </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<!--footer-->
+<?php require_once "../components/footer.php"; ?>
+
+</body>
+
+</html>
