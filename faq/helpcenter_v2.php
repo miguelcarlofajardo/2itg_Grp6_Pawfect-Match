fcda5fba7fdd9cf08930bfb4423f3d02cbf17450
miguelgabrielle <99172115+miguelgabrielle@users.noreply.github.com>
2023-05-01 13:07:09 +0800

    Add files via upload

---
+++ faq/helpcenter_v2.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pawfect Match FAQ</title>
+    <style>
+        body {
+            background-color: #FEDBD0;
+            font-family: Arial, sans-serif;
+        }
+        h1 {
+            color: #FF7F50;
+            text-align: center;
+            margin-top: 50px;
+        }
+        .container {
+            margin: 0 auto;
+            width: 80%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+        .question {
+            color: #FF7F50;
+            font-size: 24px;
+            font-weight: bold;
+            margin-top: 50px;
+        }
+        .answer {
+            font-size: 18px;
+            margin-top: 20px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <h1>Pawfect Match FAQ</h1>
+    <div class="question">
+        <h2>What is the adoption process?</h2>
+        <div class="answer">
+            <p>The adoption process involves filling out an application, meeting with a counselor to discuss your lifestyle and preferences, and then selecting the pet that is right for you. After the adoption, we provide support and resources to help you and your new pet adjust to your new life together.</p>
+        </div>
+    </div>
+    <div class="question">
+        <h2>What are the adoption fees?</h2>
+        <div class="answer">
+            <p>The adoption fees vary depending on the animal and the location, but they typically range from $50 to $200. These fees help cover the cost of spaying/neutering, vaccinations, and other medical care the animal may have received while in our care.</p>
+        </div>
+    </div>
+    <div class="question">
+        <h2>Can I schedule a visit to meet the pets?</h2>
+        <div class="answer">
+            <p>Yes, we encourage potential adopters to come in and visit with the pets. This can help you get a better sense of their personality and see if they would be a good fit for your home. You can schedule a visit by contacting us through our website or by phone.</p>
+        </div>
+    </div>
+    <div class="question">
+        <h2>What kind of post-adoption support do you provide?</h2>
+        <div class="answer">
+            <p>We provide a variety of resources and support to help you and your new pet adjust to your new life together. This includes training advice, veterinary referrals, and access to our online community of pet owners. We are always available to answer any questions you may have and provide guidance as needed.</p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
