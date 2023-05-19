928c9d2d15d18a9a19b1538685a1984178559958
Miguel Fajardo <miguelcarlo.fajardo.cics@ust.edu.ph>
2023-05-20 01:23:18 +0800

    updated css files of crud system. Miguel - 05/20

--- login/crudindex.php
+++ login/crudindex.php
@@ -3,22 +3,18 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Dashboard</title>
+    <title>Pet Index</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <style>
-        .wrapper {
-            width: 600px;
-            margin: 0 auto;
-        }
 
-        table tr td:last-child {
-            width: 120px;
-        }
-    </style>
+    <link rel="stylesheet" href="../css/crudindex.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../home/img/PM-icon.png">
     <script>
         $(document).ready(function() {
             $('[data-toggle="tooltip"]').tooltip();
@@ -27,13 +23,12 @@
 </head>
 
 <body>
-    <div class="wrapper">
-        <div class="container-fluid">
+        <div class="container">
             <div class="row">
                 <div class="col-md-12">
                     <div class="mt-5 mb-3 clearfix">
-                        <h2 class="pull-left">Pet Information</h2>
-                        <a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Add New Pet</a>
+                        <h2>Pet Information <a href="create.php" class="btn"><i class="fa fa-address-book-o"></i> Add New Pet</a> </h2>
+                        
                     </div>
                     <?php
                     // Include config file
@@ -43,7 +38,7 @@
                     $sql = "SELECT * FROM information";
                     if ($result = $mysqli->query($sql)) {
                         if ($result->num_rows > 0) {
-                            echo '<table class="table table-bordered table-striped">';
+                            echo '<table class="table table-bordered table-striped table-hover">';
                             echo "<thead>";
                             echo "<tr>";
                             echo "<th>#</th>";
@@ -56,6 +51,7 @@
                             echo "<th>Pet Information</th>";
                             echo "<th>Owner Information</th>";
                             echo "<th>Image</th>"; // Add new column for image
+                            echo "<th>Edit Information</th>";
                             echo "</tr>";
                             echo "</thead>";
                             echo "<tbody>";
@@ -72,9 +68,9 @@
                                 echo "<td>" . $row['owner'] . "</td>";
                                 echo "<td><img src='" . $row['image'] . "' alt='Pet Image' style='width: 100px; height: auto;'></td>"; // Display image using img tag
                                 echo "<td>";
-                                echo '<a href="read.php?id=' . $row['id'] . '" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
-                                echo '<a href="update.php?id=' . $row['id'] . '" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
-                                echo '<a href="delete.php?id=' . $row['id'] . '" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
+                                echo '<a href="read.php?id=' . $row['id'] . '" class="icon" title="View Record" data-toggle="tooltip"><span class="fa fa-id-card"></span></a>';
+                                echo '<a href="update.php?id=' . $row['id'] . '" class="icon" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil-square-o"></span></a>';
+                                echo '<a href="delete.php?id=' . $row['id'] . '" class="icon" title="Delete Record" data-toggle="tooltip"><span class="fa fa-minus-square"></span></a>';
                                 echo "</td>";
                                 echo "</tr>";
                             }
@@ -94,7 +90,6 @@
                     ?>
 
                 </div>
-            </div>
         </div>
     </div>
 </body>
