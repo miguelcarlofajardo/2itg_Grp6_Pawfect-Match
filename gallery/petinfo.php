531422bcbfcc541f22cdac166c7af2998fd3ac71
Lance Bernardino <lance.bernardino.cics@ust.edu.ph>
2023-05-21 09:11:29 +0800

    Added petinfo frontend 05/21 Lance

---
+++ gallery/petinfo.php
@@ -0,0 +1,46 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pawfect Match</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="../css/petinfo.css">
+    <link rel="shortcut icon" href="../home/img/PM-icon.png">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@700&family=Nunito&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet"> 
+</head>
+<body>
+
+<?php require_once "../components/navbar.php"; ?>
+<p class="space">
+
+</p>
+
+<div class="container-box">
+  <div class="petimages">
+    <img class="petimg" src="images/download (1).jpg" />
+  </div>
+ 
+  <div class="product">
+    <h4>Nike Epic React Flyknit</h4>
+    <p>Women's Running Shoe</p>
+    <h2>$150</h2>
+    <p class="desc">Pet Information</p>
+    <p class="desc">Owner Information</p>
+    <div class="buttons">
+      <button class="add"><span class="adopt">Adopt Now!</span></button>
+      <button class="like"><span class="fav">♥</span></button>
+    </div>
+  </div>
+</div>
+
+
+<?php require_once "../components/footer.php"; ?>
+
+</body>
+</html>
--- gallery/petlist.php
+++ gallery/petlist.php
@@ -6,7 +6,6 @@
     <title>Pawfect Match</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/petlist.css">
     <link rel="shortcut icon" href="../home/img/PM-icon.png">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
