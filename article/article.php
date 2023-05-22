74f6d2b591e717f8b02328c7e94e9a5af4a4bf6b
Lance Bernardino <lance.bernardino.cics@ust.edu.ph>
2023-05-22 10:07:13 +0800

    Shortcut icon

--- article/article.php
+++ article/article.php
@@ -10,7 +10,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Dongle:wght@700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/article.css">
-    <link rel="shortcut icon" href="../home/img/PW-icon.png">
+    <link rel="shortcut icon" href="../home/img/PM-icon.png">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
