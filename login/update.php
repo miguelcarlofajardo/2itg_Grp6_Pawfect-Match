e34848aa40303583d18ecbee87000fe3203f9d14
Lance Bernardino <lance.bernardino.cics@ust.edu.ph>
2023-05-22 16:59:41 +0800

    Changes to filename locations

--- login/update.php
+++ login/update.php
@@ -129,7 +129,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             // Attempt to execute the prepared statement
             if ($stmt->execute()) {
                 // Records created successfully. Redirect to landing page
-                header("location: index.php");
+                header("location: profilehome.php");
                 exit();
             } else {
                 echo "Oops! Something went wrong. Please try again later.";
