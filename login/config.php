16b59f4509a75a0a53054e13536055927d150991
Lance Bernardino <lance.bernardino.cics@ust.edu.ph>
2023-05-23 09:10:21 +0800

    Database changes

--- login/config.php
+++ login/config.php
@@ -1,10 +1,8 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'pawfect match');
+define('DB_USERNAME', 'pawfectmatch123');
+define('DB_PASSWORD', 'Pawfectmatch123-');
+define('DB_NAME', 'id20796123_pawfectmatch');
 
 /* Attempt to connect to MySQL database */
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
